144029719878fcf883c3fa1f03b9a5cbc2faf67c
Loai AlFarran <loayei@gmail.com>
2021-05-17 16:24:29 -0700

    finalloai

--- src/InfoBox.js
+++ src/InfoBox.js
@@ -2,22 +2,24 @@ import { Card, CardContent, Typography } from '@material-ui/core'
 import React from 'react'
 import './InfoBox.css';
 
-function InfoBox({title, cases, total}) {
+function InfoBox({title, cases, total, ...props}) {
     return (
-        <div >
-            <Card >
-            <CardContent>
+        <div >recovered
+            <Card 
+            onClick={props.onClick}
+            className="infoCard" >
+            <CardContent >
                 {/* Title */}
-                <Typography className="infoBox__title" color="textSecondary">
-                    {title}
+                <Typography className="infoBox__title" >
+                    {title} <small>(click to visualize on map)</small>
                 </Typography>
                 
                 {/* Number of cases:Number */}
-                <h2 className="infoBox__cases">{cases}</h2>
+                <h2 className="infoBox__cases"> <small>{cases}</small> </h2>
                 
                 {/* Total:Number */}
 
-                <Typography className="infoBox__total" color="textSecondary">
+                <Typography className="infoBox__total" >
                     {total} Total
                 </Typography>
 
--- src/Map.js
+++ src/Map.js
@@ -1,15 +1,31 @@
 import React from 'react';
 import './Map.css'
-import { MapContainer as LeafletMap, TileLayer } from 'react-leaflet';
+import { MapContainer as LeafletMap, TileLayer, useMap } from 'react-leaflet';
+import { showDataOnMap } from './utilis';
 
-function Map({center, zoom}) {
+function Map({countries, casesType, center, zoom}) {
+
+    function ChangeView({center,zoom}) {
+        const map = useMap();
+        map.setView(center,zoom);
+
+        return null;
+    }
+
+
     return (
         <div className="map">
             <LeafletMap center={center} zoom={zoom}>
+                <ChangeView center={center} zoom={zoom} />
                 <TileLayer
                 url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-                // attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
                 />
+
+                {/* Loop through the countries and draw circles */}
+
+                {showDataOnMap(countries, casesType)}
+
             </LeafletMap>
         </div>
     )
